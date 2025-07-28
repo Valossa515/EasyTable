54218a379c50f8e426ee83a83b21478e719e5c45
Valossa515 <fe.mmo515@gmail.com>
2025-07-27 22:15:15 -0300

    Initial commit

---
+++ src/main/java/br/com/EasyTable/API/EasyTableApplication.java
@@ -0,0 +1,13 @@
+package br.com.EasyTable.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyTableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/EasyTable/API/EasyTableApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.EasyTable.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
